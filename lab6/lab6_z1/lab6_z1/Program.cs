6e20569d72e572dc7541152a567b0c472d0d6b06
Szymon Sawczuk <szymon@Szymons-MacBook-Air.local>
2022-11-15 00:01:51 +0100

    Add results 1-3

---
+++ lab6/lab6_z1/lab6_z1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace lab6_z1
+{
+    class Program
+    {
+        public static void printTuple((string, string, int, double)  tuple)
+        {
+            Console.WriteLine($"Name: {tuple.Item1}, Surname: {tuple.Item2}, Age: {tuple.Item3}, Salary: {tuple.Item4:0.00}PLN");
+
+            (string Name, string Surname, int Age, double Salary) tuple2 = tuple;
+            Console.WriteLine($"Name: {tuple2.Name}, Surname: {tuple2.Surname}, Age: {tuple2.Age}, Salary: {tuple2.Salary:0.00}PLN");
+
+            var (name, surname, age, salary) = tuple;
+            Console.WriteLine("Name: {0}, Surname: {1}, Age: {2}, Salary: {3:0.00}PLN", name, surname, age, salary);
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            var info = ("Szymon", "Sawczuk", 20, 4500.00);
+            printTuple(info);
+
+        }
+    }
+}
+
