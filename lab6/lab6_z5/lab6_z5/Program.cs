75cec8cea16b120e8cfc37bda4075bf04a9cb037
Szymon Sawczuk <szymon@Szymons-MacBook-Air.local>
2022-11-15 13:24:56 +0100

    Add result ex 5

--- lab6/lab6_z5/lab6_z5/Program.cs
+++ lab6/lab6_z5/lab6_z5/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Linq;
 
-namespace lab6_z4
+namespace lab6_z5
 {
     class Program
     {
@@ -17,18 +16,18 @@ namespace lab6_z4
 
         static public void DrawCard(string firstLine, string secondLine = "", char symbol = '!', int width = 2, int minLength = 20)
         {
-            
+
             int height = 2 * width + (secondLine != "" ? 2 : 1);
             int currentHeight = height;
 
             minLength = minLength - (2 * width) <= firstLine.Length ? firstLine.Length + 2 * (width + 1) : minLength;
-            minLength = minLength - (2 *width) <= secondLine.Length ? secondLine.Length + 2 * (width + 1) : minLength;
+            minLength = minLength - (2 * width) <= secondLine.Length ? secondLine.Length + 2 * (width + 1) : minLength;
 
-            int indexToStartFirstLine = (int) (minLength * 0.5) - (int) (firstLine.Length * 0.5);
-            int indexToStartSecondLine = (int) (minLength * 0.5) - (int) (secondLine.Length * 0.5);
+            int indexToStartFirstLine = (int)(minLength * 0.5) - (int)(firstLine.Length * 0.5);
+            int indexToStartSecondLine = (int)(minLength * 0.5) - (int)(secondLine.Length * 0.5);
 
             minLength = firstLine.Length > secondLine.Length ?
-                (minLength - width == indexToStartFirstLine + firstLine.Length ? minLength + 1 : minLength):
+                (minLength - width == indexToStartFirstLine + firstLine.Length ? minLength + 1 : minLength) :
                 minLength - width == indexToStartSecondLine + secondLine.Length ? minLength + 1 : minLength;
 
             string line = "";
@@ -36,8 +35,8 @@ namespace lab6_z4
             {
                 if (currentHeight > height - width || currentHeight <= width) line = new string(symbol, minLength);
 
-                if(currentHeight == height - width) DrawLineWithText(ref line, firstLine, width, indexToStartFirstLine, minLength);
-                if(height % 2 == 0 && currentHeight == height - width - 1) DrawLineWithText(ref line, secondLine, width, indexToStartSecondLine, minLength);
+                if (currentHeight == height - width) DrawLineWithText(ref line, firstLine, width, indexToStartFirstLine, minLength);
+                if (height % 2 == 0 && currentHeight == height - width - 1) DrawLineWithText(ref line, secondLine, width, indexToStartSecondLine, minLength);
 
 
                 Console.WriteLine(line);
@@ -48,7 +47,7 @@ namespace lab6_z4
 
         static void Main(string[] args)
         {
-            DrawCard("Szymon", "Wojciech", symbol:'x', width: 2, minLength:10);
+            DrawCard("Szymon", "Wojciech", symbol: 'x', width: 2, minLength: 10);
         }
     }
 }
