1e066677f02daa0e995dba2a82571d2fca9ae090
Szymon Sawczuk <szymon@Szymons-MacBook-Air.local>
2022-12-07 11:58:43 +0100

    Add list9 ex1-3

---
+++ lab9/lab9/lab9/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab9.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
+
---
+++ lab9/lab9/lab9/ViewModels/Article.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace lab9.ViewModels
+{
+	public enum Categories
+	{
+		Books,
+		Electronics,
+		SportEquipment,
+		Clothes
+	}
+
+
+    public class Article
+	{
+
+		public int Id { get; set; }
+
+		[Required]
+		[Display(Name="Name of product")]
+		public string Name { get; set; }
+
+		[Required]
+        [RegularExpression(@"[0-9]*\.?[0-9]+", ErrorMessage = "{0} must be a Number.")]
+		[Display(Name = "Price in PLN")]
+        [Range(0, 1_000_000_000, ErrorMessage = "Price should be in range of 0 to 1 000 000 000PLN")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
+        public float Price { get; set; }
+
+		[DataType(DataType.DateTime)]
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+        public DateTime ExpirationDate { get; set; }
+
+        [Required]
+        [DisplayFormat]
+		public List<Categories> CategoriesList { get; set; }
+
+		public String CategoriesPrint;
+
+		public void PrintCategories()
+		{
+			this.CategoriesPrint = String.Join(", ", this.CategoriesList);
+		}
+
+		public Article(int id, string name, float price, DateTime expirationDate, List<Categories> categories)
+		{
+			this.Id = id;
+			this.Name = name;
+			this.Price = price;
+			this.ExpirationDate = expirationDate;
+			this.CategoriesList = categories;
+		}
+
+		public Article()
+		{
+            this.Id = 0;
+            this.Name = "test";
+            this.Price = 12;
+            this.ExpirationDate = new DateTime(29,12,12);
+            this.CategoriesList = new List<Categories>() { Categories.Books};
+        }
+
+    
+
+    }
+
+}
+
